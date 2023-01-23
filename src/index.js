9fda85d4d17e55f374bc3dd0ae226f29fc968ffa
Azeem Olatunji <Azeemol@HA-SHEM.com>
2023-01-23 14:09:15 +0100

    Added Readme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import store from './Redux/Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />} path="/" />
+      </Routes>
+    </BrowserRouter>
+  </Provider>,
 );
